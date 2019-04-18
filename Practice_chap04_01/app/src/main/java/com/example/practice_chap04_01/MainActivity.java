5fd582818bf3adfe4a077210159c8ae1474d00c6
Marlgolro <mrtj0329@gmail.com>
2019-04-19 01:41:39 +0900

    Solution_chap04_3
    Solution_chap04_3

--- Practice_chap04_01/app/src/main/java/com/example/practice_chap04_01/MainActivity.java
+++ Practice_chap04_01/app/src/main/java/com/example/practice_chap04_01/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTitle("초간단 계산기");
         initRef();
 
         btnAdd.setOnTouchListener(new View.OnTouchListener() {
